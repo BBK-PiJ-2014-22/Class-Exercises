c8cd6c2e94744a04de72ba95923f2898740ba57c
JamieMacIver <jamie.maciver@gmail.com>
2014-10-25 14:36:13 +0100

    Pi almost complete
    existing error on the addition symbol not switching

--- Day 2/e2_18_pi.java
+++ Day 2/e2_18_pi.java
@@ -7,21 +7,32 @@ public class e2_18_pi
 double pi = 0.0;
 int number = 0;
 double divider = 1.0;
-double add = 0.0;
-
+double change = 0.0;
+boolean add = true;
 System.out.println("enter N for pi calculation");
 
 number = Integer.parseInt(System.console().readLine());
 
-System.out.println("About to do a for loop...");//debug
 
 for (int i = 0; i < number; i++){
-	System.out.println("I'm doing a for loop!");//debug
-	add = 4.0 / divider;
-	System.out.println("Add: "+add);//debug
-	pi = pi + add;
-	System.out.println("Number:" + number);
+	
+	change = 4.0 / divider;
+	
+	if (add){
+		pi = pi + change;
+	}else {
+		pi = pi - change;
+	}
+	
+	divider += 2;
+	
+	
+	System.out.println();//debug
+	System.out.println("Change: "+change);//debug
+	System.out.println("Number:" + number);//debug
 	System.out.println("Pi :"+pi);//debug
+	System.out.println("Divider :"+ divider);//debug
+	System.out.println();//debug
 }
 
 System.out.println(number);
