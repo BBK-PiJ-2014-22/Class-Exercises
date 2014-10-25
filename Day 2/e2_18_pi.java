3dd09e8e73d8a473b38bc307231fdef8a28b9ec9
JamieMacIver <jamie.maciver@gmail.com>
2014-10-25 14:31:08 +0100

    For error fixed
    error where for loop not triggering fixed

--- Day 2/e2_18_pi.java
+++ Day 2/e2_18_pi.java
@@ -7,22 +7,24 @@ public class e2_18_pi
 double pi = 0.0;
 int number = 0;
 double divider = 1.0;
+double add = 0.0;
 
 System.out.println("enter N for pi calculation");
 
 number = Integer.parseInt(System.console().readLine());
 
-System.out.println(number);
+System.out.println("About to do a for loop...");//debug
 
-String stop = System.console().readLine();
-
-
-for (int i = 0; i >= number; i++){
-	pi += 4.0 / divider; 
-	System.out.println(number);
+for (int i = 0; i < number; i++){
+	System.out.println("I'm doing a for loop!");//debug
+	add = 4.0 / divider;
+	System.out.println("Add: "+add);//debug
+	pi = pi + add;
+	System.out.println("Number:" + number);
+	System.out.println("Pi :"+pi);//debug
 }
 
 System.out.println(number);
-System.out.println(pi)
+System.out.println(pi);
 }}
 
